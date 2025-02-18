1eab9a28f817593a6ca23210ecdae44b55103e6a
Zack Starrett <30493418+basset10@users.noreply.github.com>
2025-02-18 17:58:39 -0600

    Update video_looper.py

--- Adafruit_Video_Looper/video_looper.py
+++ Adafruit_Video_Looper/video_looper.py
@@ -85,7 +85,11 @@ class VideoLooper:
         pygame.display.init()
         pygame.font.init()
         pygame.mouse.set_visible(False)
-        self._screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN|SCALED)
+        resolution = pygame.display.Info() # Get the users resolution
+        swidth = resolution.current_w
+        sheight = resolution.current_h
+        self._screen = pygame.display.set_mode((swidth, sheight), FULLSCREEN, 0, 32)
+        #self._screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN|SCALED)
         self._size = (pygame.display.Info().current_w, pygame.display.Info().current_h)
         self._bgimage = self._load_bgimage() #a tupple with pyimage, xpos, ypos
         self._blank_screen()
