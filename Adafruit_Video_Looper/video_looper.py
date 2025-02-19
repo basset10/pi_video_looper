7ec3326d961f8227195070288da997116bf88917
Zack Starrett <30493418+basset10@users.noreply.github.com>
2025-02-19 17:07:20 -0600

    Update video_looper.py

--- Adafruit_Video_Looper/video_looper.py
+++ Adafruit_Video_Looper/video_looper.py
@@ -365,7 +365,11 @@ class VideoLooper:
                 self._screen.blit(bottom_label, (bottom_x, bottom_y-10))
                 pygame.display.flip()
 
-                time.sleep(1)
+                time.sleep(0.1)
+        self._screen.fill(self._bgcolor)
+        self._screen.blit(self._bgimage[0], (self._bgimage[1], self._bgimage[2]))
+        self._screen.blit(top_label, (top_x, top_y-10))
+        self._screen.blit(bottom_label, (bottom_x, bottom_y-10))
         self._bgimage = self._load_bgimage()
         pygame.display.flip()
 
